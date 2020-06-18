ff3e66757a7650c493d7e2cc6c0ce502d90a5594
DengKe <dengke1994@126.com>
2020-06-18 16:46:28 +0800

    dk 测试

---
+++ src/main/java/com/gooinfo/purchase/bjfz2008/model/yuCeDingDan.java
@@ -0,0 +1,15 @@
+package com.gooinfo.purchase.bjfz2008.model;
+
+import lombok.Data;
+
+@Data
+public class yuCeDingDan {
+    private String beiShaDate;//备纱日期
+    private String yeWuZu;//业务组
+    private String yuCeNo;//预测订单号
+    private String yuCeDate;//预测下单日期
+    private String variety;//品种（布编）
+    private String yuCeQuanity;//预测数量
+    private String varietyId;//布编ID
+    private String sasas;
+}
